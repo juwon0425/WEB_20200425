d315ec4556544af53c726779b785c160a07bd30c
정주원 <jqk777@sungkyul.ac.kr>
2023-05-10 02:23:42 +0000

    .

--- js/login.js
+++ js/login.js
@@ -2,9 +2,19 @@ function login(){
 	let form=document.querySelector("#form_main");
 	let id=document.querySelector("#floatingInput");
 	let password=document.querySelector("#floatingPassword");
+	let check=document.querySelector("#idSaveCheck");
 	
 	form.action="../index_login.html";
 	form.method="get";
+	
+	if(check.checked==true){
+		alert("쿠키를 저장합니다");
+		setCookie("id", id.value, 1);
+		alert("쿠키 값:"+id.value);
+	}
+	else{
+		setCookie("id", id.value, 0);
+	}
 	
 	if(id.value.length===0||password.value.length===0){
 		alert("아이디와 비밀번호를 모두 입력해주세요.")
@@ -31,5 +41,20 @@ function get_id(){
 		    }
 	    } // 2중 for문 끝
 }; // 함수 끝
-alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
+alert(getParameters('id') + '님 반갑습니다!'); // 메시지 창 출력
+}
+function deleteCookie(cookieName){
+	var expireDate=new Date();
+	expireDate.setDate(expireDate.getDate()-1);
+	document.cookie=cookieName+"="; expireDate.toGMTString();
+}
+function init(){
+	let id=document.querySelector("#floatingInput");
+	let check=document.querySelector("#idSaveCheck");
+	let get_id=getCookie("id");
+	
+	if(get_id){
+		id.value=get_id;
+		check.checked=true;
+	}
 }
